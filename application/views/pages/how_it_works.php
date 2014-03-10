5275fb7fe6f572f41c177a56faf3b96d6cd6d53f
Ashish Kumar Basak <ashish021@gmail.com>
2014-03-10 17:32:26 +0600

    few translation fix

--- application/views/pages/how_it_works.php
+++ application/views/pages/how_it_works.php
@@ -15,27 +15,27 @@
 		<div class="ten columns">
 		
 
-		<h1>10 Good Reasons To Publish On Trip-bangladesh!</h1>
+		<h1>10 Good Reasons To Publish!</h1>
 
-<p class="tooltips">
-1. Expose their offers in an international showcase (not just the structure). <br> 
-2. Differentiate yourself from the competition with their own proposals. <br> 
-3. Should achieve regular guests and potential customers without - annoying - newsletters. <br> 
-4. Queries related to offers sent directly to the structure. <br> 
-5. Promotion and positioning of tenders in 3 languages ​​in 
-6. Possibility to contact foreign operators in the sector (agencies, tour operators) <br> 
-7. Avvelarsi expert advice on local marketing. <br> 
-8. No registration costs and a very low annual fee. <br> 
-9. Autonomous management of web-based tool (profile, publication offers) <br> 
-10. Payment by the guests only at the property. <br>
-</p>
+		<p class="tooltips">
+			1. Expose their offers in an international showcase (not just the structure). <br> 
+			2. Differentiate yourself from the competition with their own proposals. <br> 
+			3. Should achieve regular guests and potential customers without - annoying - newsletters. <br> 
+			4. Queries related to offers sent directly to the structure. <br> 
+			5. Promotion and positioning of tenders in 3 languages ​​in 
+			6. Possibility to contact foreign operators in the sector (agencies, tour operators) <br> 
+			7. Avvelarsi expert advice on local marketing. <br> 
+			8. No registration costs and a very low annual fee. <br> 
+			9. Autonomous management of web-based tool (profile, publication offers) <br> 
+			10. Payment by the guests only at the property. <br>
+		</p>
 
 	<div class="clearfix-border"></div>
 								<div class="clearfix"></div>
-<p>Register now and create your free profile, easy and quick. <a href="<?php echo base_url();?><?php echo $this->config->item('hotel_owner_signup_url'); ?>">Puoi ideare e pubblicare un numero illimitato di offerte a solo 99��� all'anno.</a> Clicca su "Registrati" nella parte alta del sito. Si apre una semplice maschera d'inserimento. Metti i dati indispensabili (nome, cognome, e-mail, user password) per aprire e gestire il tuo spazio su Travelly (account). <em>Riceverai una e-mail di conferma dell'avvenuta registrazione. Clicca su link che trovi nella mail per attivare il tuo account.</em> <a href="/assets/pdf/travelly_come_funziona.pdf" target="_blank"><strong>Istruzioni per l'uso (scarica pdf)</strong></a></p>
+<p>Register now and create your free profile, easy and quick. <a href="<?php echo base_url();?><?php echo $this->config->item('hotel_owner_signup_url'); ?>">You can create and publish an unlimited number of offers just 1999 per year.</a> Click on "Sign up" at the top of the page. This opens a simple form to enter. Put the necessary data (name, surname, e-mail address, username, password) to open and manage your account in Trip-bangladesh.  <em>You will receive an e-mail confirming your registration. Click on the link provided in the email to activate your account.</em></p>
 	<div class="clearfix-border"></div>
 								<div class="clearfix"></div>
-			<a href="<?php echo base_url();?><?php echo $this->config->item('hotel_owner_signup_url'); ?>" class="button medium red">Registrarsi ora &raquo;</a> 
+			<a href="<?php echo base_url();?><?php echo $this->config->item('hotel_owner_signup_url'); ?>" class="button medium btn-red">Sign up now &raquo;</a> 
 			
 				</div>
 			<div class="clearfix-small"></div>
